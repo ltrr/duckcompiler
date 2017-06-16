bac10aca8e753a14d606ad52105160aa9458917c
Ronnypetson <rsronnypetson4@gmail.com>
2017-06-16 13:57:31 -0300

    dictionary

--- codegen/dictinit.cpp
+++ codegen/dictinit.cpp
@@ -15,13 +15,13 @@ tuple4_vec DictInit::genCode(context c){
     std::string id_addr = genAddr();
     context t_id_context(id_addr,c.break_label,c.continue_label);
     t_id_context.mode = Mode::Lit;
-    tuple4_vec t_id_code = t_id->genCode(context(id_addr,c.break_label,c.continue_label));
+    tuple4_vec t_id_code = t_id->genCode(t_id_context);
     dcode.insert(end(dcode),begin(t_id_code),end(t_id_code));
     //
     dcode.push_back(tuple4("setindex",dict_addr,id_addr,expr_addr));
     if(dict_init != NULL){  // dictinit is just one (key : value) pair
         std::string dictinit_addr = genAddr();
-        tuple4_vec dictinit_code = dict_init->genCode(context(dictinit_addr,c.break_label,c.continue_label,c.obj_addr));
+        tuple4_vec dictinit_code = dict_init->genCode(context(dictinit_addr,c.break_label,c.continue_label,dict_addr));
         dcode.insert(end(dcode),begin(dictinit_code),end(dictinit_code));
     }
     return dcode;
